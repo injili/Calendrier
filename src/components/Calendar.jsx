bb646dcee6688319d4034b1bbaf53ff2671ec58d
injili <nyarekigospel@gmail.com>
2024-07-13 08:51:12 +0300

    Friday work

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -210,31 +210,85 @@ export default function Calendar() {
                   leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                   leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                 >
-                  <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                  <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-max sm:p-6">
                     <div>
                       <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
                         <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
                       </div>
                       <div className="mt-3 text-center sm:mt-5">
-                        <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
-                          Add Event
+                        <Dialog.Title as="h3" className="mb-8 text-base font-semibold leading-6 text-gray-900">
+                          Make Reservation.
                         </Dialog.Title>
                         <form action="submit" onSubmit={handleSubmit}>
-                          <div className="mt-2">
-                            <input type="text" name="title" className="block w-full rounded-md border-0 py-1.5 text-gray-900 
-                            shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 
-                            focus:ring-2 
-                            focus:ring-inset focus:ring-violet-600 
-                            sm:text-sm sm:leading-6"
-                              value={newEvent.title} onChange={(e) => handleChange(e)} placeholder="  Title" />
+                          <div className="flex items-center gap-4">
+                            <label htmlFor="guest" className="font-semibold w-16 text-left">Guest</label>
+                            <input id="guest" type="text" name="guest" 
+                            className="
+                              px-2 block w-full rounded-md border-0 py-1 text-gray-900 
+                              shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 
+                              focus:ring-1
+                              focus:ring-inset focus:ring-violet-600 
+                              sm:text-sm sm:leading-6
+                            "
+                              value={newEvent.title} onChange={(e) => handleChange(e)} placeholder="Name of the guest" />
                           </div>
-                          <div className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
+                          <div className="mt-2 flex items-center gap-4">
+                            <label htmlFor="phone" className="font-semibold w-16 text-left">Phone</label>
+                            <input id="phone" type="tel" name="phone" 
+                            className="
+                              px-2 py-1 block w-full rounded-md border-0 text-gray-900 
+                              shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 
+                              focus:ring-2 focus:ring-inset focus:ring-violet-600 
+                              sm:text-sm sm:leading-6
+                            "
+                              placeholder="0712345678"
+                              />
+                          </div>
+                          <div className="mt-2 flex items-center gap-4">
+                            <label htmlFor="villas" className="font-semibold w-16 text-left">Villas</label>
+                            <input id="villas" type="number" name="villas" 
+                            className="
+                              px-2 py-1 block w-full rounded-md border-0 text-gray-900 
+                              shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 
+                              focus:ring-2 focus:ring-inset focus:ring-violet-600 
+                              sm:text-sm sm:leading-6
+                            "
+                              placeholder="1" min="1" max="3"
+                              />
+                          </div>
+                          <div className="flex items center gap-4">
+                            <div className="mt-2 flex items-center gap-2">
+                              <label htmlFor="checkin" className="font-semibold">Check-In</label>
+                              <input id="checkout" type="date" name="checkin" 
+                              className="
+                                px-2 py-1 block rounded-md border-0 text-gray-900 
+                                shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 
+                                focus:ring-2 focus:ring-inset focus:ring-violet-600 
+                                sm:text-sm sm:leading-6
+                              "
+                                placeholder="" min="" max=""
+                                />
+                            </div>
+                            <div className="mt-2 flex items-center gap-2">
+                              <label htmlFor="checkout" className="font-semibold">Check-Out</label>
+                              <input id="checkout" type="date" name="checkout" 
+                              className="
+                                px-2 py-1 block rounded-md border-0 text-gray-900 
+                                shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 
+                                focus:ring-2 focus:ring-inset focus:ring-violet-600 
+                                sm:text-sm sm:leading-6
+                              "
+                                placeholder="" min="" max=""
+                                />
+                            </div>
+                          </div>
+                          <div className="mt-8 sm:mt-8 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
                             <button
                               type="submit"
                               className="inline-flex w-full justify-center rounded-md bg-violet-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600 sm:col-start-2 disabled:opacity-25"
                               disabled={newEvent.title === ''}
                             >
-                              Create
+                              Reserve
                             </button>
                             <button
                               type="button"
